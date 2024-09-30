e5b7e2008fccba47cbbb380fdf9c26caf6ad2eb4
Dmitryi Lavrentev <Tyuiu.LavrentevDA@outlook.com>
2024-09-30 20:17:48 +0500

    Task3

---
+++ Tyuiu.LavrentevDA.Sprint1.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.LavrentevDA.Sprint1.Task3.V16.Lib;
+namespace Tyuiu.LavrentevDA.Sprint1.Task3.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 5.53;
+            double x2 = 10.1;
+            double wait = 55.853;
+            var res = ds.CoeffOfQuadraticEquation(x1, x2);
+            Assert.AreEqual(wait, res);
+          
+        }
+    }
+}
---
+++ Tyuiu.LavrentevDA.Sprint1.Task3.V16/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.LavrentevDA.Sprint1.Task3.V16.Lib;
+namespace Tyuiu.LavrentevDA.Sprint1.Task3.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Лаврентьев Д.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыкы работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Лаврентьев Дмитрий Александрович | ИБКСб-24-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчеты и печатает результат на экране              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x1, x2;
+
+            Console.WriteLine("Введите число x1:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число x2:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CoeffOfQuadraticEquation(x1, x2));
+            Console.ReadLine();
+        }
+    }
+}
